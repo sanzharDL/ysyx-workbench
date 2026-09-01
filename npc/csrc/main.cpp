9039ce1126e572a5314d700fbf59cb0fdc75120b
sanzharDL <sanzhar.mussainov@gmail.com>
2026-09-01 11:23:20 +0500

    Simulate the two-way switch module

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Vtop.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
+int main(int argc, char **argv) {
+  Verilated::commandArgs(argc, argv);
+  Vtop *top = new Vtop();
+  while (1) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
+  }
+  delete top;
   return 0;
 }
