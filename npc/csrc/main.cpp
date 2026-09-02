584aeb2cd71ef162c7ce17a6c0a6b8443287cb14
sanzharDL <sanzhar.mussainov@gmail.com>
2026-09-02 15:32:44 +0500

    Connect the running lights to NVBoard

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,20 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <nvboard.h>
 #include "Vtop.h"
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
+
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
+}
 
 int main(int argc, char **argv) {
   Verilated::commandArgs(argc, argv);
-  Vtop *top = new Vtop();
-  while (1) {
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    assert(top->f == (a ^ b));
+
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  reset(10);
+
+  while(1) {
+    nvboard_update();
+
+    single_cycle();
   }
-  delete top;
+
+  delete &dut;
   return 0;
 }
