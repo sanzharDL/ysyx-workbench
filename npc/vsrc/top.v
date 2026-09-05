ab4f726db226e78df6bbd1e5a5d4a9abd3ca3fb8
sanzharDL <sanzhar.mussainov@gmail.com>
2026-09-05 13:22:28 +0500

    Experiment 7 Listing 21 : Distinguishing between two types of state machines with different input states

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,54 +1,41 @@
-module top(
-  input clk,
-  input rst,
-  output reg [7:0] out,
-  output reg [6:0] hex1,
-  output reg [6:0] hex2
+module top
+(
+  input   clk, in, rst,
+  output reg out
 );
-  always @(posedge clk) begin
-    if (rst) begin
-      out <= 8'd1;
-    end else begin
-      out <= {(out[0] ^ out[2] ^ out[3] ^ out[4]), out[7:1]};
-    end
-  end
-  
-  always @(*) begin
-    case(out[3:0])
-      4'h0: hex1 = 7'b000_0001; // 0
-      4'h1: hex1 = 7'b100_1111; // 1
-      4'h2: hex1 = 7'b001_0010; // 2
-      4'h3: hex1 = 7'b000_0110; // 3
-      4'h4: hex1 = 7'b100_1100; // 4
-      4'h5: hex1 = 7'b010_0100; // 5
-      4'h6: hex1 = 7'b010_0000; // 6
-      4'h7: hex1 = 7'b000_1111; // 7
-      4'h8: hex1 = 7'b000_0000; // 8
-      4'h9: hex1 = 7'b000_0100; // 9
-      4'hA: hex1 = 7'b000_1000; // A
-      4'hB: hex1 = 7'b110_0000; // b
-      4'hC: hex1 = 7'b011_0001; // C 
-      4'hD: hex1 = 7'b100_0010; // d
-      4'hE: hex1 = 7'b011_0000; // E
-      4'hF: hex1 = 7'b011_1000; // F
-    endcase
-    case(out[7:4])
-      4'h0: hex2 = 7'b000_0001; // 0
-      4'h1: hex2 = 7'b100_1111; // 1
-      4'h2: hex2 = 7'b001_0010; // 2
-      4'h3: hex2 = 7'b000_0110; // 3
-      4'h4: hex2 = 7'b100_1100; // 4
-      4'h5: hex2 = 7'b010_0100; // 5 
-      4'h6: hex2 = 7'b010_0000; // 6
-      4'h7: hex2 = 7'b000_1111; // 7
-      4'h8: hex2 = 7'b000_0000; // 8
-      4'h9: hex2 = 7'b000_0100; // 9
-      4'hA: hex2 = 7'b000_1000; // A
-      4'hB: hex2 = 7'b110_0000; // b
-      4'hC: hex2 = 7'b011_0001; // C 
-      4'hD: hex2 = 7'b100_0010; // d
-      4'hE: hex2 = 7'b011_0000; // E
-      4'hF: hex2 = 7'b011_1000; // F
-    endcase
-  end
+
+parameter[3:0] S0 = 0, S1 = 1, S2 = 2, S3 = 3,
+          S4 = 4, S5 = 5, S6 = 6, S7 = 7, S8 = 8;
+
+wire [3:0] state_din, state_dout;
+wire state_wen;
+
+SimReg#(4,0) state(clk, rst, state_din, state_dout, state_wen);
+
+assign state_wen = 1;
+
+MuxKeyWithDefault#(9, 4, 1) outMux(.out(out), .key(state_dout), .default_out(0), .lut({
+  S0, 1'b0,
+  S1, 1'b0,
+  S2, 1'b0,
+  S3, 1'b0,
+  S4, 1'b1,
+  S5, 1'b0,
+  S6, 1'b0,
+  S7, 1'b0,
+  S8, 1'b1
+}));
+
+MuxKeyWithDefault#(9, 4, 4) stateMux(.out(state_din), .key(state_dout), .default_out(S0), .lut({
+  S0, in ? S5 : S1,
+  S1, in ? S5 : S2,
+  S2, in ? S5 : S3,
+  S3, in ? S5 : S4,
+  S4, in ? S5 : S4,
+  S5, in ? S6 : S1,
+  S6, in ? S7 : S1,
+  S7, in ? S8 : S1,
+  S8, in ? S8 : S1
+}));
+
 endmodule
