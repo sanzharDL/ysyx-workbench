12bc65667091bf759d7e566058f336833298f418
sanzharDL <sanzhar.mussainov@gmail.com>
2026-09-05 10:53:32 +0500

    Experiment 6: Shift Registers and Barrel Shifters

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -8,16 +8,16 @@ static TOP_NAME dut;
 
 void nvboard_bind_all_pins(TOP_NAME* top);
 
-// static void single_cycle() {
-//   dut.clk = 0; dut.eval();
-//   dut.clk = 1; dut.eval();
-// }
-
-// static void reset(int n) {
-//   dut.rst = 1;
-//   while (n -- > 0) single_cycle();
-//   dut.rst = 0;
-// }
+//static void single_cycle() {
+//  dut.clk = 0; dut.eval();
+//  dut.clk = 1; dut.eval();
+//}
+//
+//static void reset(int n) {
+//  dut.rst = 1;
+//  while (n -- > 0) single_cycle();
+//  dut.rst = 0;
+//}
 
 int main(int argc, char **argv) {
   Verilated::commandArgs(argc, argv);
@@ -25,12 +25,12 @@ int main(int argc, char **argv) {
   nvboard_bind_all_pins(&dut);
   nvboard_init();
 
-  // reset(10);
+  //reset(10);
 
   while(1) {
     nvboard_update();
     dut.eval(); 
-    // single_cycle();
+    //single_cycle();
   }
 
   delete &dut;
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,30 +1,54 @@
 module top(
-  input [3:0] A,
-  input [3:0] B,
-  input [2:0] sel,
-  output reg [3:0] out
+  input clk,
+  input rst,
+  output reg [7:0] out,
+  output reg [6:0] hex1,
+  output reg [6:0] hex2
 );
-  int n = 4;
-  wire [3:0] t_add_Cin;
-  wire Carry;
-  wire [3:0] Result;
-  wire Overflow;
-  wire Cin = (sel == 3'b001 || sel == 3'b110 || sel == 3'b111) ? 1'b1 : 1'b0;
-  assign t_add_Cin =( {4{Cin}}^B )+ Cin;  //  在这里请注意^运算和+运算的顺序
-  assign { Carry, Result } = A + t_add_Cin;
-  assign Overflow = (A[n-1] == t_add_Cin[n-1]) && (Result [n-1] != A[n-1]);
-
+  always @(posedge clk) begin
+    if (rst) begin
+      out <= 8'd1;
+    end else begin
+      out <= {(out[0] ^ out[2] ^ out[3] ^ out[4]), out[7:1]};
+    end
+  end
+  
   always @(*) begin
-    case (sel)
-      3'b000: out = Result;
-      3'b001: out = Result;
-      3'b010: out = A ^ {4{1'b1}};
-      3'b011: out = A & B;
-      3'b100: out = A | B;
-      3'b101: out = A ^ B;
-      3'b110: out = {3'b0, Result[3] ^ Overflow};
-      3'b111: out = {3'b0, Result == 4'd0};
+    case(out[3:0])
+      4'h0: hex1 = 7'b000_0001; // 0
+      4'h1: hex1 = 7'b100_1111; // 1
+      4'h2: hex1 = 7'b001_0010; // 2
+      4'h3: hex1 = 7'b000_0110; // 3
+      4'h4: hex1 = 7'b100_1100; // 4
+      4'h5: hex1 = 7'b010_0100; // 5
+      4'h6: hex1 = 7'b010_0000; // 6
+      4'h7: hex1 = 7'b000_1111; // 7
+      4'h8: hex1 = 7'b000_0000; // 8
+      4'h9: hex1 = 7'b000_0100; // 9
+      4'hA: hex1 = 7'b000_1000; // A
+      4'hB: hex1 = 7'b110_0000; // b
+      4'hC: hex1 = 7'b011_0001; // C 
+      4'hD: hex1 = 7'b100_0010; // d
+      4'hE: hex1 = 7'b011_0000; // E
+      4'hF: hex1 = 7'b011_1000; // F
+    endcase
+    case(out[7:4])
+      4'h0: hex2 = 7'b000_0001; // 0
+      4'h1: hex2 = 7'b100_1111; // 1
+      4'h2: hex2 = 7'b001_0010; // 2
+      4'h3: hex2 = 7'b000_0110; // 3
+      4'h4: hex2 = 7'b100_1100; // 4
+      4'h5: hex2 = 7'b010_0100; // 5 
+      4'h6: hex2 = 7'b010_0000; // 6
+      4'h7: hex2 = 7'b000_1111; // 7
+      4'h8: hex2 = 7'b000_0000; // 8
+      4'h9: hex2 = 7'b000_0100; // 9
+      4'hA: hex2 = 7'b000_1000; // A
+      4'hB: hex2 = 7'b110_0000; // b
+      4'hC: hex2 = 7'b011_0001; // C 
+      4'hD: hex2 = 7'b100_0010; // d
+      4'hE: hex2 = 7'b011_0000; // E
+      4'hF: hex2 = 7'b011_1000; // F
     endcase
   end
-
 endmodule
